b3a7eeeeaf38736f8ac07090081a51a99f51d62d
WULIN <wuggfox@fox.com>
2023-04-21 08:31:21 +0800

    2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pytest
 if __name__ == '__main__':
     # 不要加-s参数，allure将stdout输出到allure报告，加了只会输出到console里
     # 用例执行步骤中会有一个stdout附件记录单个用例执行过程中的stdout
-    pytest.main(["-v", "./test_case/", "--alluredir", "./result", "--clean-alluredir"])
-    shutil.copy("environment.properties", "./result")
-    os.system('allure generate ./result/ -o ./report_allure/ --clean')
+    pytest.main(["-v", "./test_case/", "--alluredir", "./allure-results", "--clean-alluredir"])
+    shutil.copy("environment.properties", "./allure-results")
+    os.system('allure generate ./allure-results/ -o ./report_allure/ --clean')
     # os.system('allure serve result')
